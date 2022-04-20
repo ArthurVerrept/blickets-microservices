ebfdb9c42a59cea0385d6a6912971b3f99d604ff
ArthurVerrept <Arthur-Verrept@hotmail.co.uk>
2022-04-20 16:40:58 +0100

    better error handling was actually worse

--- services/blockchain/src/main.ts
+++ services/blockchain/src/main.ts
@@ -3,7 +3,6 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { Transport } from '@nestjs/microservices'
 import { BlockchainServiceName, BlockchainServicePath } from 'proto-npm'
-import { HttpErrorIntercept } from './common/interceptors/httpError.interceptor'
 
 const logger = new Logger('Main')
 
@@ -18,7 +17,6 @@ const microserviceOptions = {
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice(AppModule, microserviceOptions)
-  app.useGlobalInterceptors(new HttpErrorIntercept())
   await app.listen()
   logger.verbose('Blockchain service is listening...')
 }
